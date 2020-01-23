d467a784a900e913533d35e5cacbbcb46fb34c99
greko <greko11maister@gmail.com>
2020-01-23 18:55:29 -0500

    Se agrego la conexion entre el componente results con el store, tambien se agrego el componente AppBar

---
+++ src/components/appBar/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Page from './page';
+
+class IAppBar extends React.Component {
+    render() {
+        return(
+            <Page/>
+        );
+    }
+}
+
+export default IAppBar;
--- src/components/results/index.js
+++ src/components/results/index.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 import Page from './page';
 
 class Results extends Component {
     render(){
+        const {suggestions} = this.props;
+        console.log(suggestions);
+
         return (
             <Page/>
         );
     }
 }
+const mapStateToProps = (state) => {
+        return {
+            suggestions: state.suggestions
+        };
+}
 
-export default  Results;
+/*
+//->  Hacemos el connect que nos regresa una funcion.
+const wraper = connect(mapStateToProps);
+//->  a esa funcion le pasamos nuestro componente [Results] y nos regresa otro componente
+const componnet = wraper(Results);*/
+
+export default  connect(mapStateToProps)(Results);
