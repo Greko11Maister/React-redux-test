4e0ab71c602915d9b100ccd5cc229a0f444c6847
greko <greko11maister@gmail.com>
2020-01-23 19:57:48 -0500

    se conecto el AppBar con las acciones para despachar la buscqueda de sugerencias

--- src/components/appBar/index.js
+++ src/components/appBar/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import Page from './page';
+import { connect } from 'react-redux';
+import findSuggestions from '../../redux/actions/findSuggestions';
 
 class IAppBar extends React.Component {
 
-    onChangeText(text) {
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            text : ''
+        }
+
+        this.onChangeText = this.onChangeText.bind(this);
+        this.onChangeSelection = this.onChangeSelection.bind(this);
+    }
+
+    onChangeText(text) {
+        this.setState({ text });
+        this.props.findSuggestions(text);
     }
 
     onChangeSelection(text){
@@ -12,13 +26,37 @@ class IAppBar extends React.Component {
     }
 
     render() {
+        const { text } = this.state;
+        const { suggestions } = this.props;
+
         return(
             <Page
+            text={text}
+            suggestions={suggestions}
             onChangeText={this.onChangeText}
             onChangeSelection={this.onChangeSelection}
             />
         );
     }
 }
+const mapStateToProps = (state) => {
+    return {
+        suggestions: state.suggestions
+    }
+}
 
-export default IAppBar;
+/*
+//--> Forma larga de definir la acciones.
+const mapDispatchToProps = (dispatch) => {
+    return  {
+        findSuggentions: text => dispatch(findSuggestions(text))
+    }
+}*/
+
+// Forma corta de definir las acciones
+
+const mapDispatchToProps = {
+    findSuggestions
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(IAppBar);
